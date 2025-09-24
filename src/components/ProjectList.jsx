18dd105f8c9e0abe4e92eb88eb9a7e32b5f622da
Mary Grace Rellora <relloma@MFCGD.COM>
2025-09-24 02:26:31 -0400

    Project List fetch only and style

--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -1,22 +1,79 @@
-import { useState } from "react";
-import TableRow from "./TableRow";
+import { useState, useEffect } from "react";
+import { gql } from '@apollo/client';
+import { useQuery } from '@apollo/client/react';
+
+const GET_PROJECTS = gql`
+    query Employees($projectName: String) {
+        projects(projectName: $projectName) {
+            projectName
+            description
+            startDate
+            members
+        }
+}`;
 
 function ProjectList() {
+    const [projects, setProjects] = useState([]);
+    const [selectedProject, setSelectedProject] = useState(null);
+
+    const { data, refetch } = useQuery(GET_PROJECTS, {
+        variables: { projectName: "" }, // initial value
+        fetchPolicy: 'network-only',
+        });
+
+    useEffect(() => { 
+        if (data?.projects) setProjects(data.projects); 
+    }, [data]);
+
+    const handleSelectProjectNameEvent = (event) => {
+        const projectName = event.target.value;
+        console.log('projectName:', projectName);
+        refetch({ projectName });
+    }
+
+    const handleProjectClick = (project) => {
+        setSelectedProject(project);
+    }
+
     return (
-        <>
-        <h1>Projects</h1>
-        <table>
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                </tr>
-            </thead>
-            <tbody>
+        <div className="project-list-container">
+            <h1>Projects</h1>
+            
+            <div className="table-section">
+                <div className="filter-section">
+                    <label htmlFor="projectDropdown">Filter by Project Name:</label>
+                    <input type="text" id="projectDropdown" name="projectName" onChange={(event) => handleSelectProjectNameEvent(event)} />
+                </div>
                 
-            </tbody>
-        </table>
-    </>
+                <div className="table-container">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Project Name</th>
+                                <th>Description</th>
+                                <th>Start Date</th>
+                                <th>Members</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {projects.map((project, index) => {
+                                return (
+                                    <tr 
+                                        key={index}
+                                        className="clickable-row"
+                                    >
+                                        <td>{project.projectName}</td>
+                                        <td>{project.description}</td>
+                                        <td>{project.startDate}</td>
+                                        <td>{project.members.join(", ")}</td>
+                                    </tr>
+                                )
+                            })}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
     );
 }
 
